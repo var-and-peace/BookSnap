387b93f1560f3f36ad35d10a00df5020375a9f82
Michael Maurer <maumike87@gmail.com>
2020-10-09 11:55:52 -0400

    Initial Commit

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'none',
+  semi: false,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  tabWidth: 2,
+  printWidth: 80,
+  jsxSingleQuote: true,
+  arrowParens: 'always',
+  parser: 'flow'
+};
