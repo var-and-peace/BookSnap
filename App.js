346f01a096e76edbfa5b3a9981b5044ccf49ccfb
Michael Maurer <maumike87@gmail.com>
2020-10-10 09:19:06 -0400

    chore: formatting

--- App.js
+++ App.js
@@ -5,93 +5,91 @@
  * @format
  * @flow strict-local
  */
-import React from 'react';
-import { View, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
-import { RNCamera } from 'react-native-camera';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import React from 'react'
+import { View, TouchableOpacity, StyleSheet, Dimensions } from 'react-native'
+import { RNCamera } from 'react-native-camera'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     display: 'flex',
     flexDirection: 'column',
-    backgroundColor: 'black',
+    backgroundColor: 'black'
   },
   preview: {
     flex: 1,
     justifyContent: 'flex-end',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   topButtons: {
     flex: 1,
-    width: Dimensions.get('window').width,
-    alignItems: 'flex-start',
+    alignItems: 'flex-start'
   },
   bottomButtons: {
     flex: 1,
-    width: Dimensions.get('window').width,
     justifyContent: 'flex-end',
-    alignItems: 'center',
+    alignItems: 'center'
   },
 
   flipButton: {
     flex: 1,
-    marginTop: 20,
-    right: 20,
-    alignSelf: 'flex-end',
+    alignSelf: 'flex-end'
   },
   recordingButton: {
-    marginBottom: 10,
-  },
-});
+    marginBottom: 0
+  }
+})
 
 class PhotoCamera extends React.PureComponent {
   state = {
-    type: RNCamera.Constants.Type.back,
-  };
+    type: RNCamera.Constants.Type.back
+  }
 
   flipCamera = () =>
     this.setState({
       type:
         this.state.type === RNCamera.Constants.Type.back
           ? RNCamera.Constants.Type.front
-          : RNCamera.Constants.Type.back,
-    });
+          : RNCamera.Constants.Type.back
+    })
 
   takePhoto = async () => {
-    // const { onTakePhoto } = this.props;
     const options = {
       quality: 0.5,
-      base64: true,
-    };
-    const data = await this.camera.takePictureAsync(options);
-    console.log(data.uri);
-  };
+      base64: true
+    }
+    const data = await this.camera.takePictureAsync(options)
+    console.log(data.uri)
+  }
   render() {
-    const { type } = this.state;
+    const { type } = this.state
     return (
       <View style={styles.container}>
         <RNCamera
-          ref={cam => {
-            this.camera = cam;
+          ref={(cam) => {
+            this.camera = cam
           }}
           type={type}
           style={styles.preview}
           captureAudio={false}
         />
-        <View style={styles.topButtons}>
-          <TouchableOpacity onPress={this.flipCamera} style={styles.flipButton}>
-            <Icon name="refresh" size={35} color="orange" />
-          </TouchableOpacity>
-        </View>
+        {/* <View style={styles.topButtons}> */}
+        {/* </View> */}
         <View style={styles.bottomButtons}>
-          <TouchableOpacity onPress={this.takePhoto} style={styles.recordingButton}>
-            <Icon name="camera" size={50} color="orange" />
+          <TouchableOpacity
+            onPress={this.takePhoto}
+            style={styles.recordingButton}
+          >
+            <Icon name='camera' size={50} color='orange' />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this.flipCamera} style={styles.flipButton}>
+            <Icon name='refresh' size={35} color='orange' />
           </TouchableOpacity>
         </View>
       </View>
-    );
+    )
   }
 }
 
-export default PhotoCamera;
+export default PhotoCamera
