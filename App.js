eac6a8315a6b1377ccebfd58c4dc5530487ae863
Michael Maurer <maumike87@gmail.com>
2020-10-11 18:05:11 -0400

    feat: saves picture info on state

--- App.js
+++ App.js
@@ -5,10 +5,89 @@
  * @format
  * @flow strict-local
  */
+
 import React from 'react'
 import { View, TouchableOpacity, StyleSheet, Dimensions } from 'react-native'
 import { RNCamera } from 'react-native-camera'
 import Icon from 'react-native-vector-icons/FontAwesome'
+
+class PhotoCamera extends React.PureComponent {
+  state = {
+    type: RNCamera.Constants.Type.back,
+    wordList: [],
+    base64: ''
+  }
+
+  flipCamera = () =>
+    this.setState({
+      type:
+        this.state.type === RNCamera.Constants.Type.back
+          ? RNCamera.Constants.Type.front
+          : RNCamera.Constants.Type.back
+    })
+
+  takePhoto = async (data) => {
+    const options = {
+      quality: 0.5,
+      base64: true,
+    }
+    const picture = await this.camera.takePictureAsync(options)
+    const pictureLocation = picture.uri.split('//')[1]
+    console.log('HERE IS WHERE THE PICTURE IS LOCATED', pictureLocation)
+    this.setState({ base64: picture.base64 })
+  }
+
+  onTextFound = (data) => {
+    let foundWords = []
+    if (data && data.textBlocks && data.textBlocks.length > 0) {
+      for (let i = 0; i < data.textBlocks.length; i++) {
+        let text = data.textBlocks[i].value
+        if(text && text.trim().length > 0) {
+          let words = text.split(/[\s,.?]+/)
+          if(words && words.length > 0) {
+            for (let j = 0; j < words.length; j++) {
+              if(words[j].trim().length > 0) {
+                foundWords.push(words[j])
+              }
+            }
+          }
+        }
+      }
+    this.setState({ wordList: foundWords })
+    console.log(this.state)
+    }
+  }
+
+  render() {
+    const { type } = this.state
+    return (
+      <View style={styles.container}>
+        <RNCamera
+          ref={(cam) => {
+            this.camera = cam
+          }}
+          type={type}
+          style={styles.preview}
+          captureAudio={false}
+          onTextRecognized={(data) => this.onTextFound(data)}
+        />
+        <View style={styles.bottomButtons}>
+          <TouchableOpacity
+            onPress={(data) => this.takePhoto(data)}
+            style={styles.recordingButton}
+          >
+            <Icon name='camera' size={50} color='orange' />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this.flipCamera} style={styles.flipButton}>
+            <Icon name='refresh' size={35} color='orange' />
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
+  }
+}
+
+export default PhotoCamera
 
 const styles = StyleSheet.create({
   container: {
@@ -40,55 +119,3 @@ const styles = StyleSheet.create({
     alignSelf: 'center'
   }
 })
-
-class PhotoCamera extends React.PureComponent {
-  state = {
-    type: RNCamera.Constants.Type.back
-  }
-
-  flipCamera = () =>
-    this.setState({
-      type:
-        this.state.type === RNCamera.Constants.Type.back
-          ? RNCamera.Constants.Type.front
-          : RNCamera.Constants.Type.back
-    })
-
-  takePhoto = async () => {
-    const options = {
-      quality: 0.5,
-    }
-    const data = await this.camera.takePictureAsync(options)
-    const fileLocation = data.uri.split('//')[1]
-    console.log(fileLocation)
-  }
-  render() {
-    const { type } = this.state
-    return (
-      <View style={styles.container}>
-        <RNCamera
-          ref={(cam) => {
-            this.camera = cam
-          }}
-          type={type}
-          style={styles.preview}
-          captureAudio={false}
-          onTextRecognized={(data) => console.log(data.textBlocks)}
-        />
-        <View style={styles.bottomButtons}>
-          <TouchableOpacity
-            onPress={this.takePhoto}
-            style={styles.recordingButton}
-          >
-            <Icon name='camera' size={50} color='orange' />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={this.flipCamera} style={styles.flipButton}>
-            <Icon name='refresh' size={35} color='orange' />
-          </TouchableOpacity>
-        </View>
-      </View>
-    )
-  }
-}
-
-export default PhotoCamera
