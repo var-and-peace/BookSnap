30b05e5149595e7662f74f2d2cd04a7eeda477fc
Nico <nico@hbcafe.com>
2020-10-13 16:31:00 -0400

    Made a proof of concept for the data visualization

--- src/Components/Graph.js
+++ src/Components/Graph.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { VictoryBar, VictoryChart } from 'victory-native';
+
+const data = [
+  {quarter: 1, earnings: 13000},
+  {quarter: 2, earnings: 16500},
+  {quarter: 3, earnings: 14250},
+  {quarter: 4, earnings: 19000}
+];
 
 class Graph extends React.Component {
     render(){
@@ -11,6 +19,12 @@ class Graph extends React.Component {
               alignItems: "center"
             }}>
             <Text>Welcome to Graphs!</Text>
+            <VictoryChart >
+              <VictoryBar data={data}
+                x="quarter"
+                y="earnings"
+              />
+            </VictoryChart>
           </View>
         )
     }
