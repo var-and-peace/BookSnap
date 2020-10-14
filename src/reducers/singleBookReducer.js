de239516868c429d9b9e2b21dfdc5e22f0586d41
Matt Chang <matthewhansol@gmail.com>
2020-10-14 14:29:20 -0400

    fix: final library component fix

--- src/reducers/singleBookReducer.js
+++ src/reducers/singleBookReducer.js
@@ -23,18 +23,14 @@ export const getBook = () => async (dispatch, getState) => {
 }
 export const setBook = bookId => async dispatch => {
     try {
-        console.log('SETTING BOOK')
-        let book = {};
-        if (bookId === 1) book = {
-            id: 1,
-            title: 'Great Gatsby',
-            author: 'F. Scott Fitzgerald'
-        }
-        else book = {
-            id: 2,
-            title: 'Catcher in the Rye',
-            author: 'J.D. Salinger'
-        }
+        let book = {}
+
+        if (bookId === 1) book = { id: 1, title: 'Great Gatsby', author: 'F. Scott Fitzgerald'}
+        else if (bookId === 2) book = { id: 2, title: 'Catcher in the Rye', author: 'J.D. Salinger'}
+        else if (bookId === 3) book = { id: 3, title: 'A Brave New World', author: 'Alduous Huxley'}
+        else if (bookId === 4) book = { id: 4, title: 'Slaughterhouse Five', author: 'Kurt Vonnegut'}
+        else if (bookId === 5) book = { id: 5, title: 'Howl', author: 'Allen Ginsburg'}
+
         dispatch(gotBook(book))
     } catch (err) {
         console.error(err)
