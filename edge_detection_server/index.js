3c67168c6c1f4f68814ab12252effb8e97062fba
Christopher Moon <61075793+supercow112@users.noreply.github.com>
2020-10-14 08:56:19 -0700

    ADD: edge detection script

--- edge_detection_server/index.js
+++ edge_detection_server/index.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
-
-// will use ngrok to listen to localhost from phone
+const { PythonShell } = require('python-shell')
 
 // logging middleware
 app.use(morgan('dev'))
 
 // body parsing middleware
-app.use(express.json())
+app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res, next) => {
@@ -19,9 +18,25 @@ app.get('/', (req, res, next) => {
   }
 })
 
-app.get('/phone', (req, res, next) => {
+app.put('/phone', async (req, res, next) => {
   try {
-    console.log('RECEIVED')
+    // let options = {
+    //   mode: 'text',
+    //   pythonOptions: ['-u'], // get print results in real-time
+    //   args: [req.body.base64]
+    // }
+    // PythonShell.run('script.py', options, function (err, results) {
+    //   if (err) throw err
+    //   // results is an array consisting of messages collected during execution
+    //   console.log('results: %j', results)
+    // })
+    let pyshell = new PythonShell('script.py')
+    pyshell.send(req.body.base64)
+    pyshell.on('message', function (message) {
+      // received a message sent from the Python script (a simple "print" statement)
+      console.log(message)
+    })
+    console.log('closing WARNING')
     res.sendStatus(200)
   } catch (error) {
     console.error(error)
--- edge_detection_server/script.py
+++ edge_detection_server/script.py
@@ -1,19 +1,64 @@
-# script.py
-my_name = 'Carlos'
-my_age = 18 # not a lie
-my_height = 172 # cm
-my_weight = 71 # kg
-my_eyes = 'Brown'
-my_teeth = 'White'
-my_hair = 'Black'
+import base64
+import io
+import cv2
+from imageio import imread
+import numpy as np
 
-print ("Let's talk about %s." % my_name)
-print ("He's %d centimeters tall." % my_height)
-print ("He's %d kilograms heavy." % my_weight)
-print ("Actually that's not too heavy.")
-print ("He's got %s eyes and %s hair." % (my_eyes, my_hair))
-print ("His teeth are usually %s depending on the coffee." % my_teeth)
+b64_string = input()
+img = imread(io.BytesIO(base64.b64decode(b64_string)))
 
-# this line is tricky, try to get it exactly right
-print ("If I add %d, %d, and %d I get %d. I don't know what that means but, whatever." % (
-    my_age, my_height, my_weight, my_age + my_height + my_weight))
+
+p_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+p_img = cv2.medianBlur(p_img, 5)
+p_img = cv2.Canny(p_img, 100, 150, apertureSize=3)
+
+
+lines = cv2.HoughLines(p_img, 1, np.pi/180, 150)
+filtered_lines = []
+for line in lines:
+    rho, theta = line[0]
+    if not ((0 <= theta < 0.25) or (np.pi - 0.25 < theta <= np.pi)):
+        continue
+    filtered_lines.append((rho, theta))
+
+# sort by rho
+filtered_lines.sort()
+
+# # batching
+# # batch close lines together
+# # take the average distance of the lines
+# batches = [[]]
+# MIN_LENGTH = 100/2
+# for i in range(len(filtered_lines)):
+#     rho1, theta1 = filtered_lines[i]
+#     rho0, theta0 = filtered_lines[i - 1]
+
+#     if (rho1 - rho0) < MIN_LENGTH:
+#         batches[-1].append((rho1, theta1))
+#     else:
+#         batches.append([(rho1, theta1)])
+
+# # batch processing
+# filtered_lines = [batch[len(batch)//2] for batch in batches]
+
+
+for rho, theta in filtered_lines:
+
+    a = np.cos(theta)
+    b = np.sin(theta)
+    x0 = a*rho
+    y0 = b*rho
+    x1 = rho/np.cos(theta)
+    x2 = x1 - img.shape[0]*np.tan(theta)
+    y1, y2 = 0, img.shape[0]
+    x1, x2 = int(x1), int(x2)
+
+    cv2.line(img, (x1, y1), (x2, y2), (154, 205, 50), 3)
+
+
+dsize = (3000, 2000)
+img = cv2.resize(img, dsize)
+cv2.imshow('houghlines3.jpg', img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
