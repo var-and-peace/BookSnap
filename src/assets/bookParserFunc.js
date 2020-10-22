9728d7b99d65882a8cd3dce8aebb6eff0d5ffa43
Matt Chang <matthewhansol@gmail.com>
2020-10-22 16:16:22 -0400

    fix: book-parse accepts empty author field

--- src/assets/bookParserFunc.js
+++ src/assets/bookParserFunc.js
@@ -6,10 +6,11 @@ export default (book) => {
   const ISBN_CODE = book.volumeInfo.industryIdentifiers
   const AVERAGE_RATING = book.volumeInfo.averageRating
   const GENRES = book.volumeInfo.categories
+  const AUTHORS = book.volumeInfo.authors
   return {
     BookId: `${book.id}`,
     title: toTitleCase(book.volumeInfo.title),
-    author: book.volumeInfo.authors.map(author => toTitleCase(author)),
+    author: AUTHORS ? AUTHORS.map(author => toTitleCase(author)) : ['Unknown'],
     ISBN: ISBN_CODE ? ISBN_CODE[0].identifier : null,
     coverImage: IMAGE_URL ? IMAGE_URL.thumbnail : null,
     genres: GENRES ? GENRES.map((genre) => toTitleCase(genre)) : 'Unlisted',
