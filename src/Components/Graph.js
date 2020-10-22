153c03533481e34143b3c4e25a7d9378e49d767f
Nico <nico@hbcafe.com>
2020-10-22 17:40:49 -0400

    fix: manually adding colors to bar graph

--- src/Components/Graph.js
+++ src/Components/Graph.js
@@ -69,9 +69,11 @@ class Graph extends React.Component {
         return obj
       })
     }
+    const colors = ["brown","red", "orange", "yellow","lime", "green", "cyan", "blue","purple", "violet"]
     if(idy === 1){//if it's a bar chart
       for(let i = 0; i < data.length; i++){
         data[i].count = i + 1;
+        data[i].color = colors[i];
       }
     }
     const width = Dimensions.get('window').width
@@ -116,7 +118,7 @@ class Graph extends React.Component {
                   <VictoryBar
                     horizontal
                     data={data}
-                    style={{ data:  { fill: (d) => d.fill, opacity: (d) => d.opacity }}}
+                    style={{ data: { fill: (d) => d.color, opacity: (d) => d.opacity }}}
                     animate={{ duration: 800, easing: 'bounce' }}
                     labels={({ datum }) => datum.xValue}
                     labelComponent={<VictoryLabel renderInPortal dx={10} />}
