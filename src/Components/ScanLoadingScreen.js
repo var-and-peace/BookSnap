0a4c2d7457b8e28fe9c43c983b678e577cd423ed
Christopher Moon <sc.chrismoon@gmail.com>
2020-10-23 12:23:57 -0700

    UPDATE: successful callback to update quote index with styling

--- src/Components/ScanLoadingScreen.js
+++ src/Components/ScanLoadingScreen.js
@@ -1,63 +1,85 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import { Animated, Text, View, StyleSheet, Button } from 'react-native'
 import AnimatedEllipsis from 'react-native-animated-ellipsis'
 
 const quotes = [
-  'Rivers know this: there is no hurry. We shall get there some day.',
-  "'Patience, grasshopper,' said Maia. 'Good things come to those who wait.'",
-  'The two hardest tests on the spiritual road are the patience to wait for the right moment and the courage not to be disappointed with what we encounter.',
-  'Trees that are slow to grow bear the best fruit.',
+  {
+    quote: 'Rivers know this: there is no hurry. We shall get there some day.',
+    author: 'Winnie-the-Pooh',
+  },
+  {
+    quote:
+      'The two hardest tests on the spiritual road are the patience to wait for the right moment and the courage not to be disappointed with what we encounter.',
+    author: 'Paulo Coelho',
+  },
+  {
+    quote: 'Trees that are slow to grow bear the best fruit.',
+    author: 'Moliere',
+  },
 ]
 const ScanLoadingScreen = () => {
   // fadeAnim will be used as the value for opacity. Initial Value: 0
   const fadeAnim = useRef(new Animated.Value(0)).current
-  let currIndex = 0
+
+  let [currIndex, nextIndex] = useState(0)
+
+  //   let currIndex = 0
   useEffect(() => {
-    Animated.loop(
-      Animated.sequence([
-        Animated.delay(1000),
-        Animated.timing(fadeAnim, {
-          toValue: 1,
-          duration: 2000,
-          useNativeDriver: true,
-        }),
-        Animated.timing(fadeAnim, {
-          toValue: 0,
-          duration: 2000,
-          useNativeDriver: true,
-        }),
-      ])
-    ).start(() => {
-      currIndex = (currIndex + 1) % quotes.length
+    Animated.sequence([
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: 2000,
+        useNativeDriver: true,
+      }),
+      Animated.delay(3000),
+      Animated.timing(fadeAnim, {
+        toValue: 0,
+        duration: 2000,
+        useNativeDriver: true,
+      }),
+    ]).start(() => {
+      //   currIndex = (currIndex + 1) % quotes.length
       console.log('inside loop', currIndex)
+      nextIndex((currIndex + 1) % quotes.length)
     })
-  }, [])
+  }, [currIndex])
 
   return (
-    <View style={styles.container}>
+    <View style={style.container}>
       <AnimatedEllipsis />
       <Animated.View
         style={{
           opacity: fadeAnim, // Bind opacity to animated value
         }}
       >
-        <Text style={styles.fadingText}>{quotes[currIndex]}</Text>
+        <View style={style.quoteContainer}>
+          <Text style={style.quote}>"{quotes[currIndex].quote}"</Text>
+          <Text style={style.author}>- {quotes[currIndex].author}</Text>
+        </View>
       </Animated.View>
     </View>
   )
 }
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#fff1e6',
   },
-  fadingContainer: {
-    paddingVertical: 8,
-    paddingHorizontal: 16,
-    backgroundColor: 'powderblue',
+  quoteContainer: {
+    // borderWidth: 2,
+    // position: 'relative',
+    // top: 200,
+  },
+  quote: {
+    width: 250,
+    color: 'gray',
+  },
+  author: {
+    textAlign: 'right',
+    color: 'gray',
   },
 })
 
