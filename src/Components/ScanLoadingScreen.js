b02508c0646c428be3d858689587efe336095a48
Christopher Moon <sc.chrismoon@gmail.com>
2020-10-23 08:48:55 -0700

    UPDATE: no iterations with callback function upon each loop end

--- src/Components/ScanLoadingScreen.js
+++ src/Components/ScanLoadingScreen.js
@@ -1,99 +1,17 @@
-// import React, { useRef, useEffect, useState } from 'react'
-// import { View, Text, Animated } from 'react-native'
-// import Spinner from 'react-native-loading-spinner-overlay'
-// import AnimatedEllipsis from 'react-native-animated-ellipsis'
-// import * as Animatable from 'react-native-animatable'
-
-// const quotes = [
-//   'Patience is next to Godliness.',
-//   'Please wait while we shred your books',
-//   'Your scanned books will soon be detextified',
-// ]
-
-// /*
-
-// Animated.loop(
-//   Animated.sequence([
-//     Animated.delay(3000),
-//     Animated.timing(this.state.width, {
-//       toValue: 400,
-//       duration: 2000
-//     })
-//   ]),
-//   {
-//     iterations: 10
-//   }
-// ).start()
-// */
-
-// const ScanLoadingScreen = () => {
-//   console.disableYellowBox = true
-//   //   let [currIndex, nextIndex] = useState(0)
-//   //   useEffect(() => {
-//   //     const interval = setInterval(
-//   //       () => nextIndex((currIndex + 1) % quotes.length),
-//   //       3000
-//   //     )
-//   //     return () => {
-//   //       clearInterval(interval)
-//   //     }
-//   //   })
-
-//   const currIndex = 0
-//   const textOpacity = 0
-//   Animated.loop(
-//     Animated.sequence([
-//       Animated.delay(3000),
-//       Animated.timing(textOpacity, {
-//         toValue: 400,
-//         duration: 2000,
-//       }),
-//     ]),
-//     {
-//       iterations: 10,
-//     }
-//   ).start()
-
-//   console.log('RENDERS', currIndex)
-//   return (
-//     <View style={style.screen}>
-//       <Animated.Text style={{ opacity: textOpacity }}>Hello</Animated.Text>
-//       {/* <AnimatedEllipsis useNativeDriver={true} /> */}
-//       {/* <Animatable.View
-//         animation='fadeIn'
-//         iterationCount={Infinity}
-//         duration={3000}
-//         useNativeDriver={true}
-//       >
-//         <Text>{quotes[currIndex]}</Text>
-//       </Animatable.View> */}
-//       {/* <Spinner
-//         visible={false}
-//         textContent={'Loading...'}
-//         // textStyle={styles.spinnerTextStyle}
-//       /> */}
-//       {/* <Text>Hello</Text> */}
-//     </View>
-//   )
-// }
-
-// export default ScanLoadingScreen
-
-// const style = {
-//   screen: {
-//     backgroundColor: '#fff1e6',
-//     height: '100%',
-//     // borderWidth: 2,
-//   },
-// }
-
 import React, { useRef, useEffect } from 'react'
 import { Animated, Text, View, StyleSheet, Button } from 'react-native'
+import AnimatedEllipsis from 'react-native-animated-ellipsis'
 
+const quotes = [
+  'Rivers know this: there is no hurry. We shall get there some day.',
+  "'Patience, grasshopper,' said Maia. 'Good things come to those who wait.'",
+  'The two hardest tests on the spiritual road are the patience to wait for the right moment and the courage not to be disappointed with what we encounter.',
+  'Trees that are slow to grow bear the best fruit.',
+]
 const ScanLoadingScreen = () => {
   // fadeAnim will be used as the value for opacity. Initial Value: 0
   const fadeAnim = useRef(new Animated.Value(0)).current
-
+  let currIndex = 0
   useEffect(() => {
     Animated.loop(
       Animated.sequence([
@@ -108,63 +26,23 @@ const ScanLoadingScreen = () => {
           duration: 2000,
           useNativeDriver: true,
         }),
-      ]),
-      {
-        iterations: 10,
-      }
-    ).start()
+      ])
+    ).start(() => {
+      currIndex = (currIndex + 1) % quotes.length
+      console.log('inside loop', currIndex)
+    })
   }, [])
-
-  const fadeIn = () => {
-    // Will change fadeAnim value to 1 in 5 seconds
-    // Animated.timing(fadeAnim, {
-    //   toValue: 1,
-    //   duration: 5000,
-    // }).start()
-    Animated.loop(
-      Animated.sequence([
-        Animated.delay(1000),
-        Animated.timing(fadeAnim, {
-          toValue: 1,
-          duration: 2000,
-          useNativeDriver: true,
-        }),
-        Animated.timing(fadeAnim, {
-          toValue: 0,
-          duration: 2000,
-          useNativeDriver: true,
-        }),
-      ]),
-      {
-        iterations: 10,
-      }
-    ).start()
-  }
-
-  const fadeOut = () => {
-    // Will change fadeAnim value to 0 in 5 seconds
-    Animated.timing(fadeAnim, {
-      toValue: 0,
-      duration: 5000,
-    }).start()
-  }
 
   return (
     <View style={styles.container}>
+      <AnimatedEllipsis />
       <Animated.View
-        style={[
-          //   styles.fadingContainer,
-          {
-            opacity: fadeAnim, // Bind opacity to animated value
-          },
-        ]}
+        style={{
+          opacity: fadeAnim, // Bind opacity to animated value
+        }}
       >
-        <Text style={styles.fadingText}>Fading View!</Text>
+        <Text style={styles.fadingText}>{quotes[currIndex]}</Text>
       </Animated.View>
-      {/* <View style={styles.buttonRow}>
-        <Button title='Fade In' onPress={fadeIn} />
-        <Button title='Fade Out' onPress={fadeOut} />
-      </View> */}
     </View>
   )
 }
@@ -174,20 +52,12 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#fff1e6',
   },
   fadingContainer: {
     paddingVertical: 8,
     paddingHorizontal: 16,
     backgroundColor: 'powderblue',
-  },
-  //   fadingText: {
-  //     fontSize: 28,
-  //     textAlign: 'center',
-  //     margin: 10,
-  //   },
-  buttonRow: {
-    flexDirection: 'row',
-    marginVertical: 16,
   },
 })
 
